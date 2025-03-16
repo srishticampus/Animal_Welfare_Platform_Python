eea76bb6c1546ac060ab91b2cac2f05a75587945
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-03-16 15:18:52 +0530

    namespace added to admin panel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import admin_dashboard, manage_users, manage_petshops
 from . import views
 
+app_name = 'admin_panel'
+
 urlpatterns = [
     path("", admin_dashboard, name="admin_dashboard"),
     path("users/", manage_users, name="manage_users"),
